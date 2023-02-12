dfabdd027e7fae51ac0dfd7ee6c8ef15fe9b1943
sunilflexday <sunil.kumar1@weareflexday.com>
2023-02-12 18:08:59 +0530

    fix:error fixed

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,10 @@ import Section4 from "../components/section4";
 export default function Home() {
   return (
     <Format>
-      <Section1></Section1>
+      {/* <Section1></Section1>
       <Section2></Section2>
-      <Section3></Section3>
-      <Section4></Section4>
+      <Section3></Section3> */}
+      {/* <Section4></Section4> */}
     </Format>
   );
 }
