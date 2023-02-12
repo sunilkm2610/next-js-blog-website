006d048bba84e10432546d7cd47d74c2de05e473
sunilflexday <sunil.kumar1@weareflexday.com>
2023-02-12 18:01:33 +0530

    fix:error fixed

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <Section1></Section1>
       <Section2></Section2>
       <Section3></Section3>
-      {/* <Section4></Section4> */}
+      <Section4></Section4>
     </Format>
   );
 }
